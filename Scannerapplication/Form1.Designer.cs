0c19a763e6236dd1be75e28adbaf8808b2389ded
Nathan Vieira <nathanvmag@gmail.com>
2018-09-13 23:49:52 -0300

    Finish Program

---
+++ Scannerapplication/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿using System.Windows.Forms;
+namespace Scannerapplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.showImages = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.pnl_capture = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic_scan = new System.Windows.Forms.PictureBox();
+            this.lbDevices = new System.Windows.Forms.ListBox();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pnl_capture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_scan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showImages
+            // 
+            this.showImages.Location = new System.Drawing.Point(242, 216);
+            this.showImages.Name = "showImages";
+            this.showImages.Size = new System.Drawing.Size(85, 23);
+            this.showImages.TabIndex = 5;
+            this.showImages.Text = "Show Images";
+            this.showImages.UseVisualStyleBackColor = true;
+            this.showImages.Visible = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(886, 5);
+            this.splitter1.TabIndex = 38;
+            this.splitter1.TabStop = false;
+            // 
+            // pnl_capture
+            // 
+            this.pnl_capture.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_capture.Controls.Add(this.linkLabel1);
+            this.pnl_capture.Controls.Add(this.label4);
+            this.pnl_capture.Controls.Add(this.button2);
+            this.pnl_capture.Controls.Add(this.label3);
+            this.pnl_capture.Controls.Add(this.textBox2);
+            this.pnl_capture.Controls.Add(this.label2);
+            this.pnl_capture.Controls.Add(this.button1);
+            this.pnl_capture.Controls.Add(this.textBox1);
+            this.pnl_capture.Controls.Add(this.label1);
+            this.pnl_capture.Controls.Add(this.pic_scan);
+            this.pnl_capture.Controls.Add(this.lbDevices);
+            this.pnl_capture.Controls.Add(this.btn_scan);
+            this.pnl_capture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_capture.Location = new System.Drawing.Point(0, 5);
+            this.pnl_capture.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnl_capture.Name = "pnl_capture";
+            this.pnl_capture.Size = new System.Drawing.Size(886, 622);
+            this.pnl_capture.TabIndex = 39;
+            this.pnl_capture.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_capture_Paint);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(214, 519);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 47);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Salvar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(15, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 21);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nome do arquivo ( adicionar .jpeg)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 143);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(318, 28);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(15, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Pasta Caminho";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(340, 59);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Alterar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(318, 28);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(78, 398);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Selecione o Scanner:";
+            this.label1.Visible = false;
+            // 
+            // pic_scan
+            // 
+            this.pic_scan.BackColor = System.Drawing.Color.LightGray;
+            this.pic_scan.Location = new System.Drawing.Point(467, 59);
+            this.pic_scan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pic_scan.Name = "pic_scan";
+            this.pic_scan.Size = new System.Drawing.Size(388, 551);
+            this.pic_scan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_scan.TabIndex = 6;
+            this.pic_scan.TabStop = false;
+            this.pic_scan.Visible = false;
+            // 
+            // lbDevices
+            // 
+            this.lbDevices.FormattingEnabled = true;
+            this.lbDevices.ItemHeight = 21;
+            this.lbDevices.Location = new System.Drawing.Point(78, 445);
+            this.lbDevices.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lbDevices.Name = "lbDevices";
+            this.lbDevices.Size = new System.Drawing.Size(101, 4);
+            this.lbDevices.TabIndex = 5;
+            this.lbDevices.Visible = false;
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_scan.Location = new System.Drawing.Point(61, 519);
+            this.btn_scan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(112, 47);
+            this.btn_scan.TabIndex = 4;
+            this.btn_scan.Text = "Scanear";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            this.btn_scan.Click += new System.EventHandler(this.btn_scan_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(472, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 21);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Prévia da imagem:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(23, 583);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(333, 17);
+            this.linkLabel1.TabIndex = 16;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Este Software foi desenvolvido por Nathan Magalhães";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(886, 627);
+            this.Controls.Add(this.pnl_capture);
+            this.Controls.Add(this.splitter1);
+            this.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scanner";
+            this.SizeChanged += new System.EventHandler(this.Home_SizeChanged);
+            this.pnl_capture.ResumeLayout(false);
+            this.pnl_capture.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_scan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+        //camera
+        private System.Windows.Forms.Button showImages;
+        private Splitter splitter1;
+        private Panel pnl_capture;
+        private PictureBox pic_scan;
+        private ListBox lbDevices;
+        private Button btn_scan;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button button2;
+        private Label label4;
+        private LinkLabel linkLabel1;
+    }
+}
+
---
+++ Scannerapplication/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WIATest;
+
+namespace Scannerapplication
+{
+    public partial class Form1 : Form
+    {
+        bool cansaveImage = false;
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            SetAllControlsFont(Controls);
+            cansaveImage = false;
+            if (args.Length>=1) textBox1.Text = args[0];
+            if (args.Length>1) textBox2.Text = args[1];
+            try
+            {
+                //get list of devices available
+                List<string> devices = WIAScanner.GetDevices();
+
+                foreach (string device in devices)
+                {
+                    lbDevices.Items.Add(device);
+                }
+                //check if device is not available
+                if (lbDevices.Items.Count == 0)
+                {
+                    MessageBox.Show("Você não possui nenhum scanner instalado","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                else
+                {
+                    lbDevices.SelectedIndex = 0;
+                }
+            }
+            catch
+            {
+
+            }
+           
+        }
+
+      
+
+        //button click event
+        private void btn_scan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get images from scanner
+                // List<Image> images = WIAScanner.Scan(((string)lbDevices.SelectedItem).Split('|')[1]);
+                List<Image> images = WIAScanner.Scan();
+                foreach (Image image in images)
+                {
+                    pic_scan.Image = image;
+                    pic_scan.BackColor = Color.Transparent;
+                    pic_scan.Show();
+                    pic_scan.SizeMode = PictureBoxSizeMode.StretchImage;
+                    cansaveImage = true;
+                    //save scanned image into specific folder
+                  //  image.Save(@"D:\" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".jpeg", ImageFormat.Jpeg);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+
+        private void Home_SizeChanged(object sender, EventArgs e)
+        {
+            int pheight = this.Size.Height - 153;
+            pic_scan.Size = new Size(pheight - 150, pheight);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if(!textBox2.Text.Contains(".jpeg"))
+            {
+                textBox2.Text += ".jpeg";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pic_scan.Image != null && !String.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                try
+                {
+                    if (!Directory.Exists(textBox1.Text)) Directory.CreateDirectory(textBox1.Text);
+                    if (File.Exists(Path.Combine(textBox1.Text, textBox2.Text))) File.Delete(Path.Combine(textBox1.Text, textBox2.Text));
+                }
+                catch
+                {
+                    MessageBox.Show("O caminho para salver o arquivo não é válido");
+                    return;
+
+                }
+                try
+                {
+                    pic_scan.Image.Save(Path.Combine(textBox1.Text, textBox2.Text), ImageFormat.Jpeg);
+                    MessageBox.Show("Imagem salva com sucesso, pressione OK para sair");
+                    Environment.Exit(0);
+                }
+                catch
+                {
+                    MessageBox.Show("Falha ao salvar a imagem, cheque se o nome e o caminho são válidos");
+                    return;
+                }
+            }
+            else if(String.IsNullOrEmpty(textBox1.Text) ||string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Você não preencheu o caminho do arquivo");
+
+            }
+            else
+            {
+                MessageBox.Show("Nenhuma imagem ainda foi escaneada");
+
+            }
+        }
+        public void SetAllControlsFont(Control.ControlCollection ctrls)
+        {
+            foreach (Control ctrl in ctrls)
+            {
+                if (ctrl.Controls != null)
+                    SetAllControlsFont( ctrl.Controls);
+
+                try { 
+                
+                    if (! (ctrl is LinkLabel))
+                    ctrl.Font = new Font("tahoma", 10);
+
+                }
+                catch
+                {
+
+                }
+                }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://nathanvmag.github.io");
+        }
+
+        private void pnl_capture_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
